5800cba96036e6889674d72059bfb260ecfe1bbb
FahrialRamadhan <90140718+FahrialRamadhan@users.noreply.github.com>
2024-09-27 20:11:29 +0700

    Pertemuan 1

---
+++ Fahrial Aufa Ramadhan_2311102241/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Membuat reader untuk input dari user
+	//reader := bufio.NewReader(os.Stdin)
+
+	nama := "Fahrial Aufa Ramadhan"
+	nim := "2311102241"
+
+	// Menampilkan prompt kepada pengguna
+	fmt.Println(nama)
+	fmt.Println(nim)
+
+	// Membaca input dari pengguna hingga baris baru (newline)
+	//nama, _ := reader.ReadString('\n')
+
+	// Menampilkan input yang diterima
+	//fmt.Println("Halo,", nama)
+}
